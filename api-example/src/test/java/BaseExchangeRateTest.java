b17b872cec5aef8429bf5d4aa0a59077a8fac893
ruslansoma <ruslansoma@epam.com>
2021-05-17 21:16:14 +0300

    Update getTodayExchangeRateAndValidateScheme() test. Updated json scheme

--- api-example/src/test/java/BaseExchangeRateTest.java
+++ api-example/src/test/java/BaseExchangeRateTest.java
@@ -1,14 +1,9 @@
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
 import util.PropertyParameters;
 
 import java.util.Collections;
-
-import io.qameta.allure.restassured.AllureRestAssured;
-import io.restassured.RestAssured;
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.filter.log.LogDetail;
-import io.restassured.http.ContentType;
-import io.restassured.specification.RequestSpecification;
 
 /**
  * The type Base exchange rate test.
@@ -24,18 +19,5 @@ public class BaseExchangeRateTest {
     public static void setUp() {
         RestAssured.baseURI = String.format(BASE_URL, API_KEY);
         RestAssured.filters(Collections.singletonList(new AllureRestAssured()));
-    }
-
-    /**
-     * Gets request spec.
-     *
-     * @return the request spec
-     */
-    protected RequestSpecification getRequestSpec() {
-        RequestSpecBuilder builder = new RequestSpecBuilder();
-        builder.log(LogDetail.ALL);
-        builder.setContentType(ContentType.JSON);
-        builder.setAccept(ContentType.JSON);
-        return builder.build();
     }
 }
